2cb6c6465d00324e1cf7b30305a2862375257379
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2022-12-01 14:48:49 +0100

    Remove unused code

--- script.js
+++ script.js
@@ -2,64 +2,12 @@ window.onload = function () {
   init();
 };
 
-let loadedModel;
 
 function init() {
   // SCENE
   const sceneInstance = new SceneInit("myCanvas");
   sceneInstance.initialize();
   sceneInstance.animate();
-
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-
-  /* 
-    // LIGHTS 
-    const light = new THREE.PointLight( 0xff0000, 1, 100 );
-    light.position.set(1050, 50, 50 );
-    scene.add( light );
-
-    var dirLight = new THREE.DirectionalLight( 0xffffff );
-    dirLight.position.set(300, 0, 0);
-    scene.add( dirLight );
-    
-    var dirLight2 = new THREE.DirectionalLight( 0xffffff );
-    dirLight2.position.set(-300, 0, 0);
-    scene.add( dirLight2 );
-
-    var dirLight3 = new THREE.DirectionalLight( 0xffffff );
-    dirLight3.position.set(0, 300, 0);
-    scene.add( dirLight3 );
-
-    var dirLight4 = new THREE.DirectionalLight( 0xffffff );
-    dirLight4.position.set( 0, -300, 0);
-    scene.add( dirLight4 );
-
-    var dirLight5 = new THREE.DirectionalLight( 0xffffff );
-    dirLight5.position.set( 0, 0, 300);
-    scene.add( dirLight5 );
-
-    var dirLight6 = new THREE.DirectionalLight( 0xffffff );
-    dirLight6.position.set( 0, 0, -300);
-    scene.add( dirLight6 );
-
-    // HELPERS 
-    const helper = new THREE.DirectionalLightHelper( dirLight, 5 );
-    scene.add( helper );
-    const helper2 = new THREE.DirectionalLightHelper( dirLight2, 5 );
-    scene.add( helper2 );
-    const helper3 = new THREE.DirectionalLightHelper( dirLight3, 5 );
-    scene.add( helper3 );
-    const helper4 = new THREE.DirectionalLightHelper( dirLight4, 5 );
-    scene.add( helper4 );
-    const helper5 = new THREE.DirectionalLightHelper( dirLight5, 5 );
-    scene.add( helper5 );
-    const helper6 = new THREE.DirectionalLightHelper( dirLight6, 5 );
-    scene.add( helper6 ); */
-
-  // SHAPE
-  const shapeGeometry = new THREE.BufferGeometry();
-  let vertices = new Float32Array([]);
 
   let loader = new THREE.GLTFLoader();
 
