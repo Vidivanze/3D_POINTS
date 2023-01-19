308e57eac18ff91e2ac78ccf9502f6993ade6400
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2023-01-19 13:41:44 +0100

    Update points generation & add mouse movement

--- script.js
+++ script.js
@@ -2,47 +2,62 @@ window.onload = function () {
   init();
 };
 
+let sceneInstance;
+let loadedModel;
+let group;
+
+//Mouse movement
+const mouse = new THREE.Vector2();
+let windowHalf = new THREE.Vector2( window.innerWidth / 2, window.innerHeight / 2 );
 
 function init() {
-  // SCENE
-  const sceneInstance = new SceneInit("myCanvas");
+  // Scene instance
+  sceneInstance = new SceneInit("myCanvas");
   sceneInstance.initialize();
   sceneInstance.animate();
 
+  group = new THREE.Group();
+
   let loader = new THREE.GLTFLoader();
-  loader.load("./assets/test_montagne.glb", (model) => {
+  loader.load("./assets/test_IO.gltf", (model) => {
     if (model) {
       model.scene.scale.setScalar(0.2);
 
-      let polygons = model.scene.children[0].children;
+      const polygons = model.scene.children[0].children;
       for (let i = 0; i < polygons.length; i ++) {
         points(polygons[i].position);
       };
+      
+      sceneInstance.scene.add( group );
       loadedModel = model.scene;
     }
   });
 
-  render();
+  document.addEventListener( 'mousemove', onMouseMove, false );
+
   animate();
+}
 
-  function animate() {
-    requestAnimationFrame(animate);
+//Points generation
+function points(position){
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( position, 3 ));
+  
+  const material = new THREE.PointsMaterial( { color: 0x888888 } );
+  const point = new THREE.Points( geometry, material );
+
+  group.add( point );
+}
+
+function onMouseMove(event) {
+  mouse.x = ( event.clientX - windowHalf.x );
+  mouse.y = ( event.clientY - windowHalf.y );
+}
+
+function animate() {
+  if(loadedModel) {
+    group.rotation.x = THREE.MathUtils.lerp(group.rotation.x, (mouse.y * Math.PI) / 3000, 0.1)
+    group.rotation.y = THREE.MathUtils.lerp(group.rotation.y, (mouse.x * Math.PI) / 4000, 0.1)
   }
-
-  function render() {
-    sceneInstance.render();
-  }
-
-  //Points
-  function points(position){
-    const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( position, 3 ));
-    
-    const material = new THREE.PointsMaterial( { color: 0x888888 } );
-    const points = new THREE.Points( geometry, material );
-
-    sceneInstance.scene.add( points );
-
-    animate();
-  }
+  requestAnimationFrame(animate);
 }
